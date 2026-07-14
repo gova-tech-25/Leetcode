020027692fc8e40ef046bf08cd2ddab53fa3d592
Govardhan Dande <govardhandande0@gmail.com>
2026-07-14 16:49:16 +0530

    Time: 0 ms (100%), Space: 10.7 MB (89.04%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution1 { // recursive dfs traversal
+public:
+    void inorder(TreeNode * root , vector<int> &ans){
+        if(root == NULL) return;
+        inorder(root->left , ans);
+        ans.push_back(root->val);
+        inorder(root->right , ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        inorder (root , ans);
+        return ans;
+    }
+};
+
+class Solution{ // Morris Traversal
+public:
+        vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>inorder;
+        TreeNode * cur = root;
+
+        while(cur != NULL){
+            if(cur->left == NULL){ // means it dont have left node 
+                inorder.push_back(cur->val);
+                cur = cur->right;
+            }
+            else{ // root has left node
+
+                TreeNode * prev = cur->left;
+                while(prev -> right && prev -> right != cur){
+                    prev = prev->right; 
+                }
+
+                if(prev -> right == NULL){
+                    prev -> right = cur;
+                    cur = cur->left;
+                }
+
+                else{
+                    prev->right = NULL;
+                    inorder.push_back(cur->val);
+                    cur = cur->right;
+                }
+
+            }
+        }
+        return inorder;
+
+    }
+ 
+
+};
