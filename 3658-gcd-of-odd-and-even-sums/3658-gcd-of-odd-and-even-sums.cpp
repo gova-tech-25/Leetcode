efa808a5b0ade40cd6ed997b105c03880608e784
Govardhan Dande <govardhandande0@gmail.com>
2026-07-15 09:34:03 +0530

    Time: 0 ms (100%), Space: 8.6 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,4 +1,14 @@
 class Solution {
+    // as sum of first n even nums = n (n + 1 ) and odd is n^2
+    // so gcd (n(n+1) , n^2) -> n * gcd (n , n+1) -> n * 1 ;
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n; 
+    }
+        
+};
+
+class Solution1 {
 public:
     int gcd(int a, int b) {
         while (b != 0) {
@@ -15,7 +25,7 @@ public:
         for(int i = 0 ; i < 2 * n ; i++){
             if(i % 2 == 0) sumEven += i;
             else sumOdd += i;
-            
+
         }
         
         return gcd(sumOdd ,sumEven);    
