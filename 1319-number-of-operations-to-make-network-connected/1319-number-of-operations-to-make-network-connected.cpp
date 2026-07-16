4dbf4896f029e5423091bbf6c919860c04648b49
Govardhan Dande <govardhandande0@gmail.com>
2026-07-16 23:04:45 +0530

    Time: 15 ms (60.59%), Space: 46.7 MB (51.09%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,66 @@
+class DisjointSet{
+   
+public:
+ vector<int>rank , parent;
+    DisjointSet(int n){
+        rank.resize(n+1 , 0);
+        parent.resize(n+1 , 0);
+        for(int i = 0 ; i <= n ; i++){
+            parent[i] = i;
+        }
+
+    }
+
+    int findUPar(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u , int v){
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+
+        if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        }
+        else if(rank[ulp_v] < rank[ulp_u]){
+            parent[ulp_v] = ulp_u;
+        }
+        else{
+
+             parent[ulp_v] = ulp_u;
+             rank[ulp_u]++;
+        }
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& edges) {
+
+         DisjointSet ds(n);
+        int cntExtras = 0;
+
+        for(auto it : edges){
+            int u = it[0];
+            int v = it[1];
+            if(ds.findUPar(u) == ds.findUPar(v)){
+                cntExtras++;
+            }
+            else
+            ds.unionByRank(u , v);
+        }
+        int nc = 0;
+
+        for(int i = 0 ; i < n ; i++ ){
+            if(ds.parent[i] == i){
+                nc++;
+            }
+        }
+        int ans = nc - 1;
+        
+        return cntExtras >= ans ? ans : -1;
+
+    }
+};
