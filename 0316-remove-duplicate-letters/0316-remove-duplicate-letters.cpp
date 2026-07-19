4b5986d13821491044baac88402a982d392f9011
Govardhan Dande <govardhandande0@gmail.com>
2026-07-19 09:31:40 +0530

    Time: 0 ms (100%), Space: 9.2 MB (84.28%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+         vector<int> last(26);
+
+        // Store last occurrence of each character
+        for (int i = 0; i < s.size(); i++) {
+            last[s[i] - 'a'] = i;
+        }
+
+        vector<bool> vis(26, false);
+        string ans;
+
+        for (int i = 0; i < s.size(); i++) {
+            char ch = s[i];
+
+            if (vis[ch - 'a'])
+                continue;
+
+            while (!ans.empty() &&
+                   ans.back() > ch &&
+                   last[ans.back() - 'a'] > i) {
+                vis[ans.back() - 'a'] = false;
+                ans.pop_back();
+            }
+
+            ans.push_back(ch);
+            vis[ch - 'a'] = true;
+        }
+
+        return ans;
+        
+    }
+};
