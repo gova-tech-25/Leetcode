7330fef1c12eb10924f3ea44d23ad2b0047877e6
Govardhan Dande <govardhandande0@gmail.com>
2026-07-31 14:23:12 +0530

    Time: 0 ms (100%), Space: 10.3 MB (87.61%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    // bool fn(vector<int>& piles , bool turn , int start , int end , int a  , int b){
+    //     int n = piles.size();
+
+    //     if(start == n /2 || end == n / 2 ){
+    //         return a > b ? true : false;
+    //     };
+        
+    //     if(turn){
+    //         turn = false;
+    //         a += piles[start];
+    //         fn(piles , turn , start , end , a , b);
+    //     }
+    //     else{
+    //         turn = true;
+    //         b += piles[];
+    //         fn(piles , turn , start , end , a , b);
+    //     }
+
+        
+    // }
+    bool stoneGame(vector<int>& piles) {
+        return true;
+        // int n = piles.size();
+        // bool turn = true;
+        // int a = 0
+        // int b = 0;
+
+        // bool res = fn(piles , turn , 0 , n -1 , a , b);
+        // return res;    
+    }
+};
