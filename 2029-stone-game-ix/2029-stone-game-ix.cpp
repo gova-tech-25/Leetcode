46576eda75e385a3146e841c30ba1a08e9ca1346
Govardhan Dande <govardhandande0@gmail.com>
2026-08-16 19:32:14 +0530

    Time: 3 ms (75.38%), Space: 131.3 MB (69.29%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0, 0, 0};
+
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
+
+        if (cnt[0] % 2 == 0) {
+            return min(cnt[1], cnt[2]) > 0;
+        }
+
+        return abs(cnt[1] - cnt[2]) > 2;
+    }
+};
+
+class Solution1 {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int n = stones.size();
+        int cnt [3] = {0 , 0  , 0};
+
+        for(int x : stones){
+            cnt[x % 3]++;
+        }
+
+        if(cnt[0] % 2 == 0){
+            return cnt[1] > 0 && cnt[2] > 0;
+        }
+
+        return abs(cnt[1] - cnt[2]) >= 3;
+        
+    }
+};
